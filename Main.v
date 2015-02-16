d29a9bdc82777facc746c0f9973e49699d23c5b5
Guillaume Claret <dev@clarus.me>
2015-02-16 14:36:36 +0100

    Beginning of a specification

--- Main.v
+++ Main.v
@@ -6,45 +6,49 @@ Require Import Computation.
 Import ListNotations.
 Import C.Notations.
 
-Definition versions_of_package (repository : LString.t) (package : LString.t)
-  : C.t (option (list LString.t)) :=
-  let package_folder := repository ++ LString.s "/" ++ package in
-  call! versions := Command.ListFiles package_folder in
-  match versions with
-  | None => ret None
-  | Some versions =>
-    ret @@ List.fold_left (fun (versions : option _) (version : LString.t) =>
-      match (versions, LString.split_limit version "." 2) with
-      | (Some versions, [_; version]) => Some (version :: versions)
-      | _ => None
-      end)
-      (versions : list LString.t) (Some [])
-  end.
+Module Packages.
+  Definition versions_of_package (repository : LString.t) (package : LString.t)
+    : C.t (option (list LString.t)) :=
+    let package_folder := repository ++ LString.s "/" ++ package in
+    call! versions := Command.ListFiles package_folder in
+    match versions with
+    | None => ret None
+    | Some versions =>
+      ret @@ List.fold_left (fun (versions : option _) (version : LString.t) =>
+        match (versions, LString.split_limit version "." 2) with
+        | (Some versions, [_; version]) => Some (version :: versions)
+        | _ => None
+        end)
+        (versions : list LString.t) (Some [])
+    end.
 
-Fixpoint versions_of_packages (repository : LString.t)
-  (packages : list LString.t)
-  : C.t (option (list (LString.t * list LString.t))) :=
-  match packages with
-  | [] => ret (Some [])
-  | package :: packages =>
-    let! versions := versions_of_package repository package in
-    let! next := versions_of_packages repository packages in
-    match (versions, next) with
-    | (Some versions, Some next) => ret @@ Some ((package, versions) :: next)
-    | _ => ret None
-    end
-  end.
+  Fixpoint versions_of_packages (repository : LString.t)
+    (packages : list LString.t)
+    : C.t (option (list (LString.t * list LString.t))) :=
+    match packages with
+    | [] => ret (Some [])
+    | package :: packages =>
+      let! versions := versions_of_package repository package in
+      let! next := versions_of_packages repository packages in
+      match (versions, next) with
+      | (Some versions, Some next) => ret @@ Some ((package, versions) :: next)
+      | _ => ret None
+      end
+    end.
 
-Definition packages (repository : LString.t)
-  : C.t (option (list (LString.t * list LString.t))) :=
-  call! packages := Command.ListFiles repository in
-  match packages with
-  | None => ret None
-  | Some packages => versions_of_packages repository packages
-  end.
+  Definition packages (repository : LString.t)
+    : C.t (option (list (LString.t * list LString.t))) :=
+    call! packages := Command.ListFiles repository in
+    match packages with
+    | None =>
+      do_call! Command.Log (LString.s "Folder " ++ repository ++ LString.s " not found.") in
+      ret None
+    | Some packages => versions_of_packages repository packages
+    end.
+End Packages.
 
 Definition main : C.t unit :=
-  let! packages := packages @@ LString.s "repo-stable" in
+  let! packages := Packages.packages @@ LString.s "repo-stable" in
   let message :=
     LString.s match packages with
     | None => "None"
